a861ad3f027a3c0dfe52f2a22afc1f9d909b623e
milan-sahoo04 <sahoomilan2004@gmail.com>
2025-10-23 15:12:46 +0530

    Added new file

--- String/stringPallindrome.java
+++ String/stringPallindrome.java
@@ -9,8 +9,19 @@ public class stringPallindrome {
         return new StringBuilder(str).reverse().toString();
     }
 
+    public static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String str = "noonn";
         System.out.println(checkPallindrome(str));
+        System.out.println(isPalindrome(str, 0, str.length() - 1));
     }
 }
